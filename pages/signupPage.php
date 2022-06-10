8b025bcf0d3ac9bd719f1cab484de0fe08054d6f
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2022-06-11 01:34:24 +0200

    updated

---
+++ pages/signupPage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.Css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include '\xampp\htdocs\FileRouge\nav.html' ?>
+    <div class="signup_container">
+        <div class="signupForm_div">
+
+            <div class="form_titles">
+                <h2>Welcome to SAVE</h2>
+                <p>Let’s set you up so you can donate your blood</p>
+                <p>Already have an account ? <a href="#">Log in</a></p>
+            </div>
+
+            <div class="formInputs_container">
+                <form class='signup_inputs' action="">
+                    <label for="">name</label> <br>
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                            <input type="text">
+                </form>
+            </div>
+        
+        </div>
+
+        <div class="signup_img">
+            <img src="../images/component.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
