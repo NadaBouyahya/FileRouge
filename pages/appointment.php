8ea04f50eec6eed2b1e879ca84b87bf3d87da75c
unknown <bouyahyanada.solicode@gmail.com>
2022-06-13 16:55:05 +0100

    updated

--- pages/appointment.php
+++ pages/appointment.php
@@ -4,10 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.Css">
     <title>Document</title>
 </head>
 <body>
     <?php include '../nav.html' ?>
+
+    <main>
+        <div id="eligibility_date">
+            <div> <img class="eligiDate_drop" src="../images/red_drop1.png" alt=""> <span>Check eligibility</span></div>
+            <div> <img id="middle_arrow_img" src="../images/drops_arrow.png" alt=""></div>
+            <div> <img class="eligiDate_drop" src="../images/gray_drop2.png" alt=""> <span>Select date </span></div>
+        </div>
+
+    </main>
     
 </body>
 </html>
